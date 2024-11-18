a1e6a89d5f620bdb729147c4b5fa95cebad7d096
jhfwa <jhfwa@MYPC92>
2024-11-18 11:10:47 +0800

    Add project files.

---
+++ CarRentalManagement/Domain/Vehicle.cs
@@ -0,0 +1,11 @@
+﻿namespace CarRentalManagement.Domain
+{
+    public class Vehicle : BaseDomainModel
+    {
+        public int Year { get; set;  }
+        public string? LicensePlateNumber { get; set; }
+        public int MakeId { get; set; }
+        public int Model { get; set; }
+        public int Colour { get; set; }
+    }
+}
